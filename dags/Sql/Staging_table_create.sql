c864b70ffce7b9b0cda03bd6f9a1fc9fbcf2713f
Ehijator Aigbokhaevbo <ehijatora@gmail.com>
2026-01-06 19:39:19 +0000

    Added UID columns to staging DBs

--- dags/Sql/Staging_table_create.sql
+++ dags/Sql/Staging_table_create.sql
@@ -7,8 +7,10 @@ drop table if exists SessionResultStaging;
 drop table if exists PitsStaging;
 drop table if exists DriversStaging;
 drop table if exists StintsStaging;
+drop table if exists LapsStaging;
 
 CREATE TABLE SessionsStaging(
+    ID uuid PRIMARY KEY DEFAULT gen_random_uuid(),    
     Meeting_key int,
     session_key int,
     location TEXT,
@@ -25,6 +27,7 @@ CREATE TABLE SessionsStaging(
     year int
 );
 CREATE TABLE RaceControlStaging(
+    ID uuid PRIMARY KEY DEFAULT gen_random_uuid(),    
     Meeting_key int,
     session_key int,
     date timestamp,
@@ -37,6 +40,7 @@ CREATE TABLE RaceControlStaging(
     message TEXT
 );
 CREATE TABLE OvertakeStaging(
+    ID uuid PRIMARY KEY DEFAULT gen_random_uuid(),    
     Meeting_key int,
     session_key int,
     overtaking_driver_number int,
@@ -45,6 +49,7 @@ CREATE TABLE OvertakeStaging(
     position int
 );
 CREATE TABLE PositionStaging(
+    ID uuid PRIMARY KEY DEFAULT gen_random_uuid(),    
     date timestamp,
     session_key int,
     position int,
@@ -52,6 +57,7 @@ CREATE TABLE PositionStaging(
     driver_number int
 ); 
 CREATE TABLE WeatherStaging(
+    ID uuid PRIMARY KEY DEFAULT gen_random_uuid(),    
     date timestamp,
     session_key int,
     wind_direction int,
@@ -64,6 +70,7 @@ CREATE TABLE WeatherStaging(
     pressure DOUBLE precision
 ); 
 CREATE TABLE SessionResultStaging(
+    ID uuid PRIMARY KEY DEFAULT gen_random_uuid(),    
     position int,
     driver_number int,
     number_of_laps int,
@@ -77,6 +84,7 @@ CREATE TABLE SessionResultStaging(
     points INT
 );
 CREATE TABLE PitsStaging(
+    ID uuid PRIMARY KEY DEFAULT gen_random_uuid(),    
     date TIMESTAMP,
     session_key int,
     driver_number int,
@@ -85,6 +93,7 @@ CREATE TABLE PitsStaging(
     lap_number INT
 );
 CREATE TABLE DriversStaging(
+    ID uuid PRIMARY KEY DEFAULT gen_random_uuid(),    
     meeting_key int,
     session_key int,
     driver_number int,
@@ -99,6 +108,7 @@ CREATE TABLE DriversStaging(
     country_code TEXT
 );
 CREATE TABLE StintsStaging(
+    ID uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     meeting_key int,
     session_key int,
     stint_number int,
@@ -109,14 +119,23 @@ CREATE TABLE StintsStaging(
     tyre_age_at_start int
 );
 CREATE TABLE LapsStaging(
+    ID uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     meeting_key int,
     session_key int,
-    stint_number int,
     driver_number int,
-    lap_start int,
-    lap_end int,
-    compound text,
-    tyre_age_at_start int
+    lap_number int,
+    date_start TIMESTAMP,
+    duration_sector_1 DOUBLE precision,
+    duration_sector_2 DOUBLE precision,
+    duration_sector_3 DOUBLE precision,
+    i1_speed int,
+    i2_speed int,
+    is_pit_out_lap bit,
+    lap_duration DOUBLE precision,
+    segments_sector_1 TEXT,
+    segments_sector_2 TEXT,
+    segments_sector_3 TEXT,
+    st_speed INT
 );
 
 
