0299719b7d36ae6eda8ac7e22ace2a34bfb87dd9
Ehijator Aigbokhaevbo <ehijatora@gmail.com>
2026-01-07 18:46:35 +0000

    Custom dag for API request WIP

--- dags/F1API_Extract.py
+++ dags/F1API_Extract.py
@@ -1,7 +1,8 @@
 import requests 
 import pandas as pd
 import airflow
-from airflow.sdk import dag, task
+import time
+from airflow.sdk import dag, task, BaseOperator
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 import datetime
@@ -9,70 +10,76 @@ import datetime
 ## will refactor into a custom operator
 
 @dag(
-    dag_id='create_required_tables',
+    dag_id='back_date_load',
     start_date=datetime(2026,1,1),
     schedule=None,
     tags=['One-time']
 )
 
-def StagingTables():
-    create_tabs = SQLExecuteQueryOperator(
-        task_id = 'create_staging_tables',
-        conn_id = 'postgres_localhost',
-        sql = 'Sql/Staging_table_create.sql')
-
 ## Returns dataframes from selected api endpoints
-class F1API:
+class F1APIOperator(BaseOperator):
     url = 'https://api.openf1.org/v1/' 
 
-    def __init__(self,start_date,end_date):
+    def __init__(self,start_date,end_date,**kwargs) -> None:
+        super().__init__(**kwargs)
         self.start_date = start_date
         self.end_date = end_date
     
-    # returns session data as dataframe
-    def sessionsdf(self):
-        url_tag = 'sessions?'
-        date = F1API.url + url_tag + 'date_start' + '>=' + self.start_date + '&' + 'date_end' + '<=' + self.end_date 
+    # possible url keys = [sessions]
+    def DateStartToDateEndFilter(self,url_key:str='sessions'):
+        url_tag = url_key + '?'
+        date = F1APIOperator.url + url_tag + 'date_start' + '>=' + self.start_date + '&' + 'date_end' + '<=' + self.end_date 
         response = requests.get(date)
         json = response.json()
         df = pd.json_normalize(json)
-        return df
+        try :
+            return df
+        except requests.exceptions.RequestException as e:
+            raise SystemExit
         
-    # takes in url key returns data as dataframe
     # possible url keys = [race_control, overtakes, position, weather]
-    def date_filter(self,url_key): 
+    def DateFilter(self,url_key:str): 
         url_tag = url_key + '?'
-        date = F1API.url + url_tag + 'date' + '>=' + self.start_date + '&' + 'date' + '<=' + self.end_date 
+        date = F1APIOperator.url + url_tag + 'date' + '>=' + self.start_date + '&' + 'date' + '<=' + self.end_date 
         response = requests.get(date)
         json = response.json()
         df = pd.json_normalize(json)
-        return df
+        try :
+            return df
+        except requests.exceptions.RequestException as e:
+            raise SystemExit
 
-    # takes in url key returns data as dataframe
     # possible url keys = [session_result, pit, drivers, stints]
-    def session_loop(self,url_key): # session_result, pit, drivers, stints 
+    def SessionKeyLoop(self,url_key:str): # session_result, pit, drivers, stints 
         url_tag = url_key + '?'
-        sessionsdf = F1API(self.start_date,self.end_date).sessionsdf()
+        sessionsdf = F1APIOperator(self.start_date,self.end_date).DateStartToDateEndFilter()
         sessionkeys = (sessionsdf["session_key"].unique())
-        #print(sessionkeys)
         data = pd.DataFrame()
         for key in sessionkeys:
-            x = requests.get(F1API.url + url_tag + 'session_key=' + str(key)).json()
+            time.sleep(1)
+            x = requests.get(F1APIOperator.url + url_tag + 'session_key=' + str(key)).json()
             stage = pd.json_normalize(x)
             data = pd.concat([data,stage],ignore_index=True)
-        return data 
+        try :
+            return data
+        except requests.exceptions.RequestException as e:
+            raise SystemExit
 
-    #return laps data as dataframe
-    def lapsdf(self,url_key): # laps 
+    # possible url keys = [laps]
+    def DateStartToDateStart(self,url_key:str ='laps'): 
         url_tag = url_key + '?'
-        date = F1API.url + url_tag + 'date_start' + '>=' + self.start_date + '&' + 'date_start' + '<=' + self.end_date 
+        date = F1APIOperator.url + url_tag + 'date_start' + '>=' + self.start_date + '&' + 'date_start' + '<=' + self.end_date 
         response = requests.get(date)
         json = response.json()
         df = pd.json_normalize(json)
-        return df
+        try :
+            return df
+        except requests.exceptions.RequestException as e:
+            raise SystemExit
 
-#session = F1API('2025-01-01','2025-12-31').lapsdf('laps')
-#print(F1API('2025-01-01','2025-05-31').session_loop('session_result'))   
+date = F1APIOperator('2025-01-01','2025-12-31')
+date.DateStartToDateEndFilter('sessions').to_sql() > time.sleep(1) > date.DateFilter('race_control')> time.sleep(1) > date.DateFilter('overtakes')> time.sleep(1)> date.DateFilter('position')> time.sleep(1)> date.DateFilter('weather')> time.sleep(1)> date.SessionKeyLoop('session_result')> time.sleep(1)> date.SessionKeyLoop('pit')> time.sleep(1)> date.SessionKeyLoop('drivers')> time.sleep(1)> date.SessionKeyLoop('stints')> time.sleep(1)> date.DateStartToDateStart('laps')
+  
         
 
 
